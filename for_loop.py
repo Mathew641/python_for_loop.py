eca60b781538fbb4536acb14c74118d0016efabe
Mathew641 <106030476+Mathew641@users.noreply.github.com>
2022-11-13 22:38:19 -0800

    Create for_loop.py

---
+++ for_loop.py
@@ -0,0 +1,62 @@
+# Here first we try to print some iterative variable using loop.
+
+'''for num in range(1,11):
+    print(num)
+'''
+# Here inside parantheses three argument are present- first one-1(included), second one-11(excluded), third one- step1(by default)
+
+'''for num in range(1,11,2):
+   print(num)'''
+# this is how we can print numbers 1 to 10 by giving 2 step jump from each number or we can also consider it as a odd number program . 
+
+# Now let's write program to print number in reversal order.
+'''for num in range(10,0,-1):
+    print(num)'''
+# as step value is always remains 1 by default, so we need to change step value argument according to our program requirement.
+
+# this is how we iterate the value using for loop but important thing is here we knew the no. of iteration.
+
+# in -- in is a keyword for the "for loop"
+
+# range ---  range is a inbuilt function in python which generating value in our program.
+# range(1,11)-- create the List containing numbers 1 to 10. List is a data type in python.
+
+
+# List data type in python is somewhat like array data type in C,C++ but the only difference is that list can comprise of heterogeneous elements unlike array.abs(x) 
+# i.e. in one list can have integer vaue, float value, char value, double value, string etc...
+
+
+
+# How d owe declare list in python, let's see..
+'''list = [1,4,4,6,8,3,5]
+print(list)
+'''
+
+#Ques.. WAP to calculate sum of given list elements using for loop?
+'''int_list1 = [1,5,8,10,-6,8,45]
+list1_sum=0
+for num in int_list1:
+    list1_sum = list1_sum + num
+
+print("Sum of all the elements in list1 is",list1_sum)'''
+
+
+        
+
+             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
